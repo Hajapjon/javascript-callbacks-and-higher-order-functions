7c50b9ae64737a37dab67fd3e50a8daac9b0c774
Wasawat-Khot <wasawat.kho@gmail.com>
2025-03-04 13:17:09 +0700

    ex.1-2

--- ex-2.js
+++ ex-2.js
@@ -1,8 +1,25 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+const studentPass70=(operation(array));
+  if(studentPass70.length>=5){
+    return "ผ่านเกณฑ์✅";
+  }
+  else if(studentPass70.length<5){
+    return "ไม่ผ่านเกณฑ์❌" ;
+  }else {
+    return "มีข้อผิดพลาด";
+  }
 }
+
+function operation(studentScoresArray){
+ const scoreArray=[];
+  for(let index of studentScoresArray){
+    if (index >=70){
+        scoreArray.push(index);
+    } 
+  }return scoreArray;
+} 
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -10,6 +27,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result="ห้องเรียนที่ 1 "+atLeastFive(studentScoresRoom1,operation) ;
+let scoreRoom2Result="ห้องเรียนที่ 2 "+atLeastFive(studentScoresRoom2,operation) ;
+let scoreRoom3Result="ห้องเรียนที่ 3 "+atLeastFive(studentScoresRoom3,operation) ;
+
+console.log(scoreRoom1Result)
+console.log(scoreRoom2Result)
+console.log(scoreRoom3Result)
