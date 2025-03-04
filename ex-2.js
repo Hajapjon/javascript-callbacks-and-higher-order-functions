75d72901c2bbe1a473ee27201c33ebabafd8dac4
nichalada <nichaladasuphachayanon@gmail.com>
2025-03-04 11:52:46 +0700

    feat: add answer in question 1-2

--- ex-2.js
+++ ex-2.js
@@ -2,7 +2,20 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0
+  for(let score of array) {
+    if (score > 70) {
+      count++;
+    }
+  }
+  return studentQuality(count)
 }
+
+const studentQuality = (count) => {
+  return count >= 5? `ผ่านเกณฑ์ ✅` : `ไม่ผ่านเกณฑ์ ❌`;
+  
+}
+
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -10,6 +23,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, studentQuality);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, studentQuality);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, studentQuality);
+
+console.log(scoreRoom1Result)
+console.log(scoreRoom2Result)
+console.log(scoreRoom3Result)
